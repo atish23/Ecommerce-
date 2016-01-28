e68bb615d9bc26b28d2251f149a77f1cb06d2d8e
blonkar <blonkar@delaplex.in>
2016-01-28 11:47:07 +0530

    changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,6 +19,9 @@ class Product < ActiveRecord::Base
      def self.active
         where({products: {active: true}}).includes(:images)
      end
-
-     
+    def featured_image(image_size = :small)
+    Rails.cache.fetch("Product-featured_image-#{id}-#{image_size}", expires_in: 3.hours) do
+      images.first ? images.first.photo.url(image_size) : "no_image_#{image_size.to_s}.jpg"
+    end
+    end
 end
